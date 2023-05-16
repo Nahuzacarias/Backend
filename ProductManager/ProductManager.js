d7eff1928e0885b4519ea8eaa4a75653b961727e
Nahuzacarias <zacarias.nahu@gmail.com>
2023-05-16 14:15:21 -0300

    v2

--- ProductManager/ProductManager.js
+++ ProductManager/ProductManager.js
@@ -1,54 +1,126 @@
+import fs from 'fs'
+
 class ProductManager {
    constructor() {
-      this.counter=1
+      this.path = "ProductManager.json"
       this.product = [];
+
    }
 
-   addProduct = (title, description, price, thumbnail,code,stock) => {
 
-     let validatecode = this.product.find((codi)=>codi.code==code)
-let id
-if(validatecode)
+   addProduct = async (title, description, price, thumbnail, code, stock) => {
+      
+      let validateCode = this.product.find((codi) => codi.code == code);
+      if (validateCode) {
+         console.log("Código repetido, favor de ingresar otro");
+      } else {
+         
+         const prod = await this.getProducts();
+         
+         
+         let producto = {
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: code,
+            stock: stock,
 
-{
-   console.log("Código repetido,favor de ingresar otro")
+            
+         };
 
-     
-   }else{
-      const id = this.counter++
+         if (prod.length === 0) {
+            producto.id = 1;
+        } else {
+            producto.id = prod[prod.length - 1].id + 1;
+        }
+         prod.push(producto);
 
-      let Producto = {
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code:code,
-      stock:stock,
-      id:id,
+         try {
+           await fs.promises.writeFile(this.path, JSON.stringify(prod, null, "\t"));
+         } catch (error) {
+            console.error("Error al escribir en el archivo:", error);
+         }
+      }
    };
-  
-   this.product.push(Producto)}
-};
 
-   getProducts = () => {
-      return (
+   getProducts = async () => {
+      try {
+         const data = await fs.promises.readFile(this.path, "utf-8");
+         if (data) {
+            const prod = JSON.parse(data);
+            return prod;
+         } else {
+            return [];
+         }
+      } catch (error) {
+         console.log(error);
+      }
+   };
 
-         this.product
-      )
+   getProductById = async (id) => {
+
+
+      JSON.parse(await fs.promises.readFile(this.path, "utf-8"))
+
+      const prod = this.product.find(item => item.id === id)
+      if (!prod) {
+         return 'Not Found'
+
+      } else {
+
+         return prod
+      }
+
    }
-getProductById= (id) =>{
+   updateProduct = async (id, title, description, price, thumbnail, code, stock) => {
+      try {
+          let producto = {
+              title: title,
+              description: description,
+              price: price,
+              thumbnail: thumbnail,
+              code: code,
+              stock: stock,
+              id: id
+          };
+          const prod = await this.getProducts();
+          const updatep= prod.findIndex(prod=>prod.id==id)
+          if(updatep !== -1){
+            prod[updatep] = producto;
 
-   const prod = this.product.find(item => item.id === id)
-   if (!prod) return 'Not Found'
-   return prod
+            await fs.promises.writeFile(this.path, JSON.stringify(prod, null, "\t"))
+          }
+else{ console.log("No se ha modificado el producto")}
+         }
+          catch (error) {
+            console.log(error)
+         }
+      }
+
+   deleteProduct = async (id) => {
+      try {
+         const prod = await this.getProducts();
+         const indexProducto = prod.findIndex(prod => prod.id == id);
+         if (indexProducto !== -1) {
+            const deleteP = prod.splice(indexProducto, 1)[0];
+            await fs.promises.writeFile(this.path, JSON.stringify(prod, null, "\t")
+            );
+            console.log("El producto fue eliminado");
+            return deleteP
+         } else {
+            return console.log("El producto que quiere eliminar no existe");
+         }
+      } catch (error) {
+         console.log(error)
+
+      }
+
+   };
 }
-   
-}
 
-const productManager = new ProductManager ()
+const productManager = new ProductManager()
 
-productManager.addProduct('RTX3050',"Placa de video con RTX",1000,"img","01",5)
-productManager.addProduct("RTC3060","Placa de video con RTX",1200,"img","02",5)
-productManager.addProduct("RTC3070","Placa de video con RTX",1400,"img","03",5)
-
-console.log(productManager.product);
+productManager.addProduct('RTX3050', "Placa de video con RTX", 1000, "img", "01", 5)
+productManager.addProduct("RTC3060", "Placa de video con RTX", 1200, "img", "02", 5)
+productManager.addProduct("RTC3070", "Placa de video con RTX", 1400, "img", "03", 5)
